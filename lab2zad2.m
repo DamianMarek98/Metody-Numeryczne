af2f02eb8610da79f8cfb83690c0094d06720cad
Damian Marek <43189598+DamianMarek98@users.noreply.github.com>
2019-03-13 13:06:00 +0100

    Update lab2zad2.m

--- lab2zad2.m
+++ lab2zad2.m
@@ -1,4 +1,6 @@
-function lab2zad2()
+
+close all
+clear all
 x=0;
 y=0;
 r=0;
@@ -8,31 +10,21 @@ drawCounter=0;
 drawCounterList=0;
 bool=0;
 
-function plot_circ(X, Y, R)
-theta = linspace(0,2*pi);
-xc = R*cos(theta) + X;
-yc = R*sin(theta) + Y;
-plot(xc,yc);
-hold on
-end
-
-function colision(x1,y1,r1,x2,y2,r2)
-    bool=sqrt((x2-x1)^2 + (y2-y1)^2) < (r1+r2);
-end
 %limits        
 a=100;
 r_max=a/2;
-n_max=50;
+n_max=100;
 var=1;
 while var<n_max+1
-  randInt=randi([1,r_max],1);
-  xa=randi([0,a],1);
-  ya=randi([0,a],1);
+  randI=rand(1)*r_max;
+  xa=rand(1)*(a);
+  ya=rand(1)*(a);
   drawCounter=drawCounter+1;
-  if(xa+randInt<a && ya+randInt<a && xa-randInt>0 && ya-randInt>0) %check if circle fit in range (0,0 to a,a)
+  if(xa+randI<a && ya+randI<a && xa-randI>0 && ya-randI>0) %check if circle fit in range (0,0 to a,a)
       iterator=1;
       for k = x %check if not there is no collision with other circle 
-          colision(xa,ya,randInt,k,y(iterator),r(iterator)); 
+         
+          bool=sqrt((k-xa)^2 + (y(iterator)-ya)^2) < (randI+r(iterator));
           if(bool==1)
               break
           end
@@ -41,19 +33,33 @@ while var<n_max+1
       if bool==0 %add circle data to vectors
       x(var)=xa;
       y(var)=ya;
-      r(var)=randInt;
-      area=area+(pi*(randInt).^2);
+      r(var)=randI;
+      area=area+(pi*(randI).^2);
       areaList(var)=area;
-      plot_circ(xa,ya,randInt);
+      plot_circ(xa,ya,randI);
       fprintf(1, ' %s%5d%s%.3g\r ', 'n =',  var, ' S = ', area)
-      var=var+1;
       drawCounterList(var)=drawCounter;
       drawCounter=0;
+      var=var+1;
       end
   end
 end
-figure('Name','Wykres','NumberTitle','off');
-semilogx(areaList);
+
+
+figure('Name','WykresPola','NumberTitle','off');
+semilogx(1:n_max,areaList);
 figure('Name','WykresLosowan','NumberTitle','off');
-loglog(drawCounterList);
+drawCounterListCumSum= cumsum(drawCounterList);
+numbers=1:n_max;
+avrage=cumsum(drawCounterList);
+
+loglog(1:n_max,avrage./numbers);
+
+function plot_circ(X, Y, R)
+theta = linspace(0,2*pi);
+xc = R*cos(theta) + X;
+yc = R*sin(theta) + Y;
+plot(xc,yc);
+hold on
 end
+
