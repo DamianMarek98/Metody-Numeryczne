10384369cff4904dbfca3059dfc75edac54514d4
Damian Marek <43189598+DamianMarek98@users.noreply.github.com>
2019-03-13 11:55:16 +0100

    Update lab2zad1.m

--- lab2zad1.m
+++ lab2zad1.m
@@ -1,13 +1,14 @@
 disp("zadInstrukcja")
-Edges=[1,1,2,2,2,3,3,3,4,4,5,5,6,6,7
-       4,6,3,4,5,5,6,7,5,6,4,6,4,7,6];
+clear all
+v = [1,1,2,2,2,3,3,3,4,4,5,5,6,6,7;
+     4,6,3,4,5,5,6,7,5,6,4,6,4,7,6];
 d=0.85;
 N=7;
-I=eye(N); %macierz jednostkowa N
-B=full(sparse(Edges(2, :),Edges(1, :), 1)); %macierz s¹siedztwa B
-A=diag(1./sum(B));
-M=I-(d*B*A);
-b=((1-d)/N)*ones(1,N);
-r=(M/b);
-r=r/norm(r);
+I=speye(N);
+B = sparse(v(2,:),v(1,:),1);
+A = sparse(diag(1./sum(B)));
+M = I-d*B*A;
+b(1:N,1)=(1-d)/N;
+
+r=M\b;
 bar(r);
